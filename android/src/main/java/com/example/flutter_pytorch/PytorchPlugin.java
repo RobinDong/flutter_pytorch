7f43155bc689ac15251a6b7d887eea3fc85eeae3
Robin Dong <rdong@woolworths.com.au>
2020-04-12 15:23:56 +1000

    Add implemention of runModelOnImage() so client could do prediction now

--- android/src/main/java/com/example/flutter_pytorch/PytorchPlugin.java
+++ android/src/main/java/com/example/flutter_pytorch/PytorchPlugin.java
@@ -3,7 +3,11 @@ package com.example.flutter_pytorch;
 import androidx.annotation.NonNull;
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
+
+import com.google.gson.Gson;
 
 import org.pytorch.IValue;
 import org.pytorch.Module;
@@ -21,9 +25,12 @@ import io.flutter.plugin.common.BinaryMessenger;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 /** PytorchPlugin */
 public class PytorchPlugin implements FlutterPlugin, MethodCallHandler {
+    static private List<Module> mModules;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -46,6 +53,10 @@ public class PytorchPlugin implements FlutterPlugin, MethodCallHandler {
         channel.setMethodCallHandler(new PytorchPlugin());
     }
 
+    public PytorchPlugin() {
+        mModules = new ArrayList<Module>();
+    }
+
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         if (call.method.equals("getPlatformVersion")) {
@@ -57,19 +68,14 @@ public class PytorchPlugin implements FlutterPlugin, MethodCallHandler {
             } catch (Exception e) {
                 result.error("Failed to load model", e.getMessage(), e);
             }
-        } /*else if (call.method.equals("runModelOnImage")) {
-        try {
-        new RunModelOnImage((HashMap) call.arguments, result).executeTfliteTask();
-        } catch (Exception e) {
-        result.error("Failed to run model", e.getMessage(), e);
-        }
-        } else if (call.method.equals("runModelOnBinary")) {
-        try {
-        new RunModelOnBinary((HashMap) call.arguments, result).executeTfliteTask();
-        } catch (Exception e) {
-        result.error("Failed to run model", e.getMessage(), e);
-        }
-        }*/ else {
+        } else if (call.method.equals("runModelOnImage")) {
+            try {
+                String res = runModelOnImage((HashMap) call.arguments);
+                result.success(res);
+            } catch (Exception e) {
+                result.error("Failed to run model", e.getMessage(), e);
+            }
+        } else {
             result.notImplemented();
         }
     }
@@ -77,10 +83,39 @@ public class PytorchPlugin implements FlutterPlugin, MethodCallHandler {
     private int loadModel(HashMap args) throws IOException {
         String modelPath = args.get("model").toString();
         Module module = Module.load(modelPath);
+        Log.v("tag", modelPath);
         if (module == null) {
             return -1;
         }
-        return 0;
+        mModules.add(module);
+        return mModules.size() - 1;
+    }
+
+    private String runModelOnImage(HashMap args) throws IOException {
+        int modelNo = (int)args.get("modelno");
+        byte[] image = (byte[])args.get("img");
+        int height = (int)args.get("height");
+        int width = (int)args.get("width");
+        String scoresJson = null;
+
+        try {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
+            // Resize bitmap
+            bitmap = Bitmap.createScaledBitmap(bitmap, width, height, false);
+
+            float[] norm_mean = {0.0f, 0.0f, 0.0f};
+            float[] norm_std = {1.0f, 1.0f, 1.0f};
+            final Tensor input = TensorImageUtils.bitmapToFloat32Tensor(bitmap, norm_mean, norm_std);
+            final Tensor output = mModules.get(modelNo).forward(IValue.from(input)).toTensor();
+            final float[] scores = output.getDataAsFloatArray();
+            // Serialize result
+            Gson gson = new Gson();
+            scoresJson = gson.toJson(scores);
+        } catch (Exception e) {
+            Log.e("FlutterPytorch", "Error reading image", e);
+            return "";
+        }
+        return scoresJson;
     }
 
     @Override
